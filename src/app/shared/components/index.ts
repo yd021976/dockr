e142b1882c18058b60116962c17644434bfa5ae9
Yann <tigrou@imac-tigrou.home>
2019-01-01 08:30:50 +0100

    Refactor menu item in side-nav component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -11,7 +11,6 @@ import { RouterModule } from '@angular/router';
 
 import { BackdropComponent } from './backdrop/backdrop.component';
 import { ErrorDialogComponent } from './error/dialog/error-dialog.component';
-import { MenuItemComponent } from './side-nav/menu-item/menu-item.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { OutletComponent } from './outlet/outlet.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
@@ -27,7 +26,6 @@ export const components = [
     SideNavComponent,
     MenuPanelComponent,
     MenuLinkComponent,
-    MenuItemComponent
 ];
 
 @NgModule({
